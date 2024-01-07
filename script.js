ddf9ce38ef2e227e1b59ba3ffd5635181e6da909
HichamBENKADA <hicham20014@gmail.com>
2024-01-07 15:03:18 -0500

    utils.j, nav.js and styles.css files are add to repo

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 console.log("Welcome to Advanced JavaScript Project")
 
+const app = document.getElementById("app");
 const getFileStream = async (url) => {
     // implementation
   };
   
   (async () => {
-    const stream = await getFileStream("https://domain.name/path/file.ext");
-    for await (const chunk of stream) {
-      console.log({ chunk });
-    }
+    app.style.width = "50%",
+    app.style.margin = "20 auto"
+    // const stream = await getFileStream("https://domain.name/path/file.ext");
+    // for await (const chunk of stream) {
+    //   console.log({ chunk });
+    // }
   })();
