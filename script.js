6cf0f4b86a8c3d70e6c8ed6d2e2a0984dd842cb5
HichamBENKADA <hicham20014@gmail.com>
2024-01-07 21:13:50 -0500

    Adding a toggle event listener to all bottons

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
 console.log("Welcome to Advanced JavaScript Project");
 
 const app = document.getElementById("app");
-const nav = document.getElementById("nav");
+const navs = document.querySelectorAll("nav");
+const btns = document.querySelectorAll("button");
+
+
+// clear active btn
+function deactivateBtn(){
+    btns.forEach((btn)=>{
+        btn.classList.remove('active')
+    })
+}
+btns.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        e.preventDefault();
+        e.stopPropagation();
+        console.log(e.target);
+        deactivateBtn();
+        e.target.classList.toggle('active');
+    })
+})
 
 app.textContent +=
   " Feel free to discover the funniest jokes and memes of all time, save your favorets and post your own.";
 
 app.style.textAlign = "left";
 
-//navigation bar
-nav.innerHTML = `
-`;
-
-const btn = document.querySelectorAll("button");
-nav.addEventListener("click", (e) => {
-  btn.classList.remove("active");
-  console.log(e.target);
-});
-
-// const getFileStream = async (url) => {
-//     // implementation
-//   };
-
-//   (async () => {
-//     document.body.style.textAlign = "center"
-//     app.style.width = "50%",
-//     app.style.margin = "20 auto"
-//     app.style.border = 1px solid black;
-
-//     // const stream = await getFileStream("https://domain.name/path/file.ext");
-//     // for await (const chunk of stream) {
-//     //   console.log({ chunk });
-//     // }
-//   })();
