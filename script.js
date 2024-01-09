7f12b08e548c14779866595a56847dd1d9ac0e94
HichamBENKADA <hicham20014@gmail.com>
2024-01-08 23:54:25 -0500

    data fetching using get random joke is checked

--- script.js
+++ script.js
@@ -1,24 +1,65 @@
 console.log("Welcome to Advanced JavaScript Project");
 
-// import * as tools from "./utils"
-
 export const app = document.getElementById("app");
 export const btns = document.querySelectorAll("button");
 
-const API_KEY = "66dfe78e14784d349f96980de7b0f3c1"
+const API_KEY = "66dfe78e14784d349f96980de7b0f3c1";
 
-axios.defaults.baseURL = 'https://api.humorapi.com';
-axios.defaults.headers.common['x-api-key'] = API_KEY;
+axios.defaults.baseURL = "https://api.humorapi.com";
+axios.defaults.headers.common["x-api-key"] = API_KEY;
 
 /**
- * Part1: fitching jockes
+ * Part1: fetching jockes
  */
 
-
-
-async function getRandomJoke(){
-    const joke = await axios.get('/jokes/random').then((res)=>{console.log(res.data)
-        app.textContent = res.data.joke
-    app.style.background = "lightgreen"})
+export const jokes = [];
+export async function getRandomJoke() {
+  try {
+    const req = await axios.get("/jokes/random")
+    .then((res) => {
+      console.log(res.data);
+      const jokeObj = res.data;
+      app.style.background = "lightgreen";
+      app.innerHTML = `${jokeObj.joke}
+        <!-- Favouriting and Voting -->
+    <div style="display:flex; justify-content:center;align-items:center;margin:5px auto">
+      <i id='voteUp' class="fa-regular fa-thumbs-up"></i>
+      <i id='fav' style="margin:auto" class="fa-regular fa-heart"></i>
+      <i id='voteDown' class="fa-regular fa-thumbs-down"></i>
+    </div>`;
+    });
+  } catch (err) {
+    console.error(err);
+  }
 }
-getRandomJoke()
+
+
+//add thumbsUp event listener
+async function thumbsUpHandler() {
+  try {
+    //post an UpVoting
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+//add thumbsDown event listener
+async function thumbsDownHandler() {
+  try {
+    //post an downVoting
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+//add Favourites Event listener
+const heartEl = document.getElementById("fav");
+// heartEl.addEventListener('click',favHandler)
+function favHandler() {
+  try {
+    heartEl.style.background = red;
+    //push to favourites
+  } catch (err) {
+    console.error(err);
+  }
+}
